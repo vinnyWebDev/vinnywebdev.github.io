36f459e739964615f1b4ed9c6585845bfffedf74
vinnyWebDev <vinnywebdev@gmail.com>
2023-04-03 11:53:18 +0100

    added 99 bottles of beer

--- script.js
+++ script.js
@@ -71,3 +71,14 @@ function repeatGame() {
     }
 }
 
+//output lyrics
+function playSong() {
+    //for loop to itterate through the song
+    for (i = 99; i >= 2; i--) {
+        let minusBeer = i - 1;
+        document.getElementById("loopSongText").innerHTML += "<br />" + i + " Bottles of beer on the wall " + i + " Bottles of beer. <br /> You take one down, you pass it aroun, <br />" + minusBeer + " Bottles of beer on the wall. <br />";
+    }
+    //not par of the loop as the lyrics here occur only once, when the song ends.
+    document.getElementById("songText").innerHTML += "One Bottle of beer on the wall, One bottle of beer <br /> You take it down you pass it around, 0 bottles of beer on the wall. <br /> No more bottles of beer on the wall, no more bottles of beer. <br />Go to the store buy some more, 99 bottles of beer on the wall!";
+
+}
